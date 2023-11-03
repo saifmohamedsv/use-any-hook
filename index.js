60cb9f31c548d9ab4956d54b320536a9dc63246f
saifmohamedsv <xcyberghostx1@gmail.com>
2023-11-03 10:51:43 +0200

    fix

--- index.js
+++ index.js
@@ -1,49 +1,22 @@
-import React, { useState, useEffect, useCallback } from "react";
-import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const useFetch = (url) => {
-  const [fetchedData, setFetchedData] = useState({
-    data: [],
-    isLoading: true,
-    error: false,
-  });
-
-  const cancelTokenSource = axios.CancelToken.source();
-
-  const fetchData = useCallback(async () => {
-    try {
-      const response = await axios.get(url, {
-        cancelToken: cancelTokenSource.token,
-      });
-
-      const data = await response.data;
-
-      if (data) {
-        setFetchedData({
-          data: data.results ? data.results : data,
-          isLoading: false,
-          error: false,
-        });
-      }
-    } catch (error) {
-      if (axios.isCancel(error)) {
-        console.log("fetching data aborted");
-      } else {
-        console.log("error occured", error);
-        setFetchedData({
-          data: [],
-          isLoading: false,
-          error: true,
-        });
-      }
-    }
-  }, [url]);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetchData();
-    return () => cancelTokenSource.cancel();
-  }, [url, fetchData]);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [url]);
 
-  const { data, isLoading, error } = fetchedData;
-  return { data, isLoading, error };
+  return { data, loading, error };
 };
